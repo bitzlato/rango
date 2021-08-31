74823e458c29369fe38e0562d7561b88c4227925
mailer21 <metelew@gmail.com>
2021-08-31 11:59:03 +0300

    stagings config

--- config/deploy/s3.rb
+++ config/deploy/s3.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-set :build_domain, 'market-s3.bitzlato.com'
 set :stage, 's3'
+set :application, -> { 'rango-' + fetch(:stage).to_s }
 
 server ENV.fetch( 'STAGING_SERVER_2' ), user: fetch(:user), roles: fetch(:roles)
