f9430c0ff5ea96c2619fc908efcf8a0d37dcaa4a
mailer21 <49911293+mailer21@users.noreply.github.com>
2021-08-31 12:21:18 +0300

    stagings config (#10)

--- config/deploy/s5.rb
+++ config/deploy/s5.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-set :stage, 's2'
+set :stage, 's5'
 set :application, -> { 'rango-' + fetch(:stage).to_s }
 
 server ENV.fetch( 'STAGING_SERVER_2' ), user: fetch(:user), roles: fetch(:roles)
