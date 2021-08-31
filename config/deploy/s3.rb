771d2f8e9acb36d3c81b878782c61e4f33db373a
Danil Pismenny <danil@brandymint.ru>
2021-08-31 11:51:38 +0300

    Add s3 stage

--- config/deploy/s3.rb
+++ config/deploy/s3.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-set :build_domain, 'market-s2.bitzlato.com'
-set :stage, 's2'
+set :build_domain, 'market-s3.bitzlato.com'
+set :stage, 's3'
 
 server ENV.fetch( 'STAGING_SERVER_2' ), user: fetch(:user), roles: fetch(:roles)
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 set :stage, :staging
-set :public_url, 'https://ex-stage.bitzlato.bz/'
 set :build_domain, 'ex-stage.bitzlato.bz'
 
 server ENV.fetch( 'STAGING_SERVER' ), user: fetch(:user), roles: fetch(:roles)
