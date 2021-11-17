78d51f28fb54d6572dac8856add3c7aeaccf0535
mailer21 <metelew@gmail.com>
2021-11-17 20:43:46 +0300

    NB! env SANDBOX_SERVER for sandbox should be used now. And it was moved from ex5 to ex10

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 set :stage, 'sandbox'
-set :application, -> { 'rango-' + fetch(:stage).to_s }
+set :application, -> { 'rango' }
 set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:stage)}/#{fetch(:application)}" }
 
-server ENV.fetch( 'STAGING_SERVER' ), user: fetch(:user), roles: fetch(:roles)
+server ENV.fetch( 'SANDBOX_SERVER' ), user: fetch(:user), roles: fetch(:roles)
