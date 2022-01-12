a69045c2b613cb3701e2c69195679049c80b6a43
Danil Pismenny <dapi@users.noreply.github.com>
2022-01-12 16:54:24 +0300

    Update jwt.go

--- tools/jwt/jwt.go
+++ tools/jwt/jwt.go
@@ -10,6 +10,7 @@ import (
 var (
 	port  = flag.String("port", "7070", "Port to bind")
 	uid   = flag.String("uid", "IDABC0000001", "UID")
+	bitzlato_uid   = flag.String("bitzlato_uid", 123, "P2P user ID")
 	email = flag.String("email", "admin@barong.io", "Email")
 	role  = flag.String("role", "admin", "Role")
 	level = flag.Int("level", 3, "Level")
@@ -19,6 +20,6 @@ func main() {
 	flag.Parse()
 
 	ks, _ := auth.LoadOrGenerateKeys("config/rsa-key", "config/rsa-key.pub")
-	t, _ := auth.ForgeToken(*uid, *email, *role, *level, ks.PrivateKey, nil)
+	t, _ := auth.ForgeToken(*bitzlato_uid, *uid, *email, *role, *level, ks.PrivateKey, nil)
 	fmt.Print(t)
 }
